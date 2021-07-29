a1741b112832e8975186fe6de2dd24e324a0eb1c
RodrigoSester <rodrigows09@gmail.com>
2021-07-29 16:41:48 -0300

    created operations with max, min, average, filter

---
+++ files/TesteOperacoes.kt
@@ -0,0 +1,24 @@
+package files
+
+fun main(){
+    val salarios = doubleArrayOf(
+        1000.0,
+        2250.0,
+        4000.0
+    )
+
+    for(salario in salarios){
+        println(salario)
+    }
+
+    println("--------------------")
+
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media dos salarios: ${salarios.average()}")
+
+    println("--------------------")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    salariosMaiorQue2500.forEach{println(it)}
+}
