0ec62d68e27a7aee1d177eeab73fda5b4ed0d771
RodrigoSester <rodrigows09@gmail.com>
2021-07-29 02:48:30 -0300

    created an ArrayOf

---
+++ files/TesteIntArrayOf.kt
@@ -0,0 +1,16 @@
+package files
+
+fun main(){
+    val values = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values.forEach {
+        println(it)
+    }
+
+    println("----------")
+
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+}
