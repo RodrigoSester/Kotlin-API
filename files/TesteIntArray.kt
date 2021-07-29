964db104b7556a0f0e78e6a114f4292595bed7a7
RodrigoSester <rodrigows09@gmail.com>
2021-07-29 02:48:07 -0300

    created an array

---
+++ files/TesteIntArray.kt
@@ -0,0 +1,31 @@
+package files
+
+fun main(){
+    val values = IntArray(5)
+    values[0] = 5
+    values[1] = 4
+    values[2] = 3
+    values[3] = 2
+    values[4] = 1
+
+    for(valor in values){
+        println(valor)
+    }
+
+    println("----------------------")
+
+    for(index in values.indices){
+        println(values[index])
+    }
+
+    println("----------------------")
+    values.forEach { valor ->
+        println(valor)
+    }
+
+    println("----------------------")
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+}
