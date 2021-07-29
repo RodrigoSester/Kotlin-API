f3267c0f49b6a8780b1045e7d343c5056b807334
RodrigoSester <rodrigows09@gmail.com>
2021-07-29 03:20:03 -0300

    created an DoubleArray

---
+++ files/TesteDoubleArray.kt
@@ -0,0 +1,22 @@
+package files
+
+fun main(){
+    val salarios = DoubleArray(3)
+    salarios[0] = 3500.0
+    salarios[1] = 5000.0
+    salarios[2] = 1000.0
+
+    salarios.forEach { println(it) }
+
+    println("--------------------------------------")
+    salarios.forEachIndexed{ index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("--------------------------------------")
+
+    val bonus = doubleArrayOf(500.0, 220.0, 1000.0)
+    bonus.sort()
+    bonus.forEach { println(it) }
+}
